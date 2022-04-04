4f1f385a85bfd7004c13e92d017e3ac4d5eefd4c
Nii <inodarteifio@gmail.com>
2022-04-04 12:55:29 -0400

    upload

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -12,7 +12,9 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def home():
+    print('hi')
     mars_db = mongo.db.collection.find_one()
+    print(mars_db)
     return render_template("index.html", mars=mars_db)
 
 
@@ -20,12 +22,13 @@ def home():
 
 @app.route("/scrape")
 def scrape():
-
+    print('start scrape')
         # Run the scrape function
-    mars_info = scrape_mars.scrape_info()
+    mars = scrape_mars.scrape_all()
 
+    print(mars.mars_facts_html)
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, mars_info, upsert=True)
+    mongo.db.collection.update({}, mars, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
@@ -33,8 +36,8 @@ def scrape():
 if __name__ == "__main__":
     app.run(debug=True)
 
-
-
+#run flask
+#$env:FLASK_ENV = "debug"; python app.py
 
 
 
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -93,4 +93,15 @@ def mars_hemisphere(browser):
         hemisphere_dict={'Title':title, 'img_url': img}
         hemisphere_image_url.append(hemisphere_dict)
 
-    return(hemisphere_image_url)
+   # return(hemisphere_image_url)
+
+    test_dict= [{'Title': 'Cerberus Hemisphere Enhanced',
+    'img_url': 'https://marshemispheres.com/images/f5e372a36edfa389625da6d0cc25d905_cerberus_enhanced.tif_full.jpg'},
+    {'Title': 'Schiaparelli Hemisphere Enhanced',
+    'img_url': 'https://marshemispheres.com/images/3778f7b43bbbc89d6e3cfabb3613ba93_schiaparelli_enhanced.tif_full.jpg'},
+    {'Title': 'Syrtis Major Hemisphere Enhanced',
+    'img_url': 'https://marshemispheres.com/images/555e6403a6ddd7ba16ddb0e471cadcf7_syrtis_major_enhanced.tif_full.jpg'},
+    {'Title': 'Valles Marineris Hemisphere Enhanced',
+    'img_url': 'https://marshemispheres.com/images/b3c7c6c9138f57b4756be9b9c43e3a48_valles_marineris_enhanced.tif_full.jpg'}]
+
+    return(test_dict)
